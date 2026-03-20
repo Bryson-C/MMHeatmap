eebfe67efdac3ad86fa96825f6dd7c048b56fba1
WhisperKnight <61612853+Bryson-C@users.noreply.github.com>
2026-03-20 12:36:51 -0600

    Initial commit

---
+++ src/main/java/org/bc/mMheatmap/MMheatmap.java
@@ -0,0 +1,17 @@
+package org.bc.mMheatmap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MMheatmap extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
